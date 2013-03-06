f96cafc7fb53443f043aef1949fe3ed781605fe6
Hayato OKUMOTO <falcon@falconsrv.net>
2013-03-06 10:13:51 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ServerWatcher::Application.config.secret_token = '49c73af66414f0fc7158336de1585b1ab905d1bd34c7859da187c12fc2afa04e1d0fc4c0ad5164518e24ef47b3c3bda8cc6c67dcbb8917508733efd58bca5a0f'
