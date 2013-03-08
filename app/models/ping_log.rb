208d0e026f50739794d0fc6cf4ec5b212f4b4efd
Hayato OKUMOTO <falcon@falconsrv.net>
2013-03-08 11:56:27 +0900

    set validations and relationships between Server and PingLogs

--- app/models/ping_log.rb
+++ app/models/ping_log.rb
@@ -1,3 +1,31 @@
 class PingLog < ActiveRecord::Base
-  attr_accessible :date, :ping_detail, :server_id, :status
+  attr_accessible :ping_detail, :server_id, :status
+
+  # Relation Ship
+  belongs_to :server
+
+  # Validations
+  validates :server_id,
+  	numericality: :only_integer
+  validates :server_id, :status,
+  	presence: true
+
+  # Scopes
+  scope :asc_by_date,
+  	lambda {
+  		order 'date ASC'
+  	}
+  scope :desc_by_date,
+  	lambda {
+  		order 'date DESC'
+  	}
+
+  # Callback
+  before_create :timestamp_date
+
+  private
+
+  def timestamp_date
+  	self.date = Time.now
+  end
 end
--- app/models/server.rb
+++ app/models/server.rb
@@ -1,3 +1,13 @@
 class Server < ActiveRecord::Base
   attr_accessible :address, :description
+
+  # Relation Ship
+  has_many :ping_logs,
+    dependent: :delete_all
+
+  # Validations
+  validates :address,
+    uniqueness: true,
+    presence: true,
+    length: {maximum: 30}
 end
