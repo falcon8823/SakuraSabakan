3a1450027ff1cbaa01f9e3d8608317d30544e055
Hayato OKUMOTO <falcon@falconsrv.net>
2013-03-09 01:21:29 +0900

    Delete access and nest resources

--- app/controllers/ping_logs_controller.rb
+++ app/controllers/ping_logs_controller.rb
@@ -1,8 +1,9 @@
 class PingLogsController < ApplicationController
-  # GET /ping_logs
-  # GET /ping_logs.json
+  # GET /servers/1/ping_logs
+  # GET /servers/1/ping_logs.json
   def index
-    @ping_logs = PingLog.desc_by_date
+    @server = Server.find(params[:server_id])
+    @ping_logs = @server.ping_logs.desc_by_date
 
     respond_to do |format|
       format.html # index.html.erb
@@ -10,10 +11,11 @@ class PingLogsController < ApplicationController
     end
   end
 
-  # GET /ping_logs/1
-  # GET /ping_logs/1.json
+  # GET /servers/1/ping_logs/1
+  # GET /servers/1/ping_logs/1.json
   def show
-    @ping_log = PingLog.find(params[:id])
+    @server = Server.find(params[:server_id])
+    @ping_log = @server.ping_logs.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
@@ -21,64 +23,15 @@ class PingLogsController < ApplicationController
     end
   end
 
-  # GET /ping_logs/new
-  # GET /ping_logs/new.json
-  def new
-    @ping_log = PingLog.new
-    @servers = Server.all
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @ping_log }
-    end
-  end
-
-  # GET /ping_logs/1/edit
-  def edit
-    @ping_log = PingLog.find(params[:id])
-    @servers = Server.all
-  end
-
-  # POST /ping_logs
-  # POST /ping_logs.json
-  def create
-    @ping_log = PingLog.new(params[:ping_log])
-
-    respond_to do |format|
-      if @ping_log.save
-        format.html { redirect_to @ping_log, notice: 'Ping log was successfully created.' }
-        format.json { render json: @ping_log, status: :created, location: @ping_log }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @ping_log.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /ping_logs/1
-  # PUT /ping_logs/1.json
-  def update
-    @ping_log = PingLog.find(params[:id])
-
-    respond_to do |format|
-      if @ping_log.update_attributes(params[:ping_log])
-        format.html { redirect_to @ping_log, notice: 'Ping log was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @ping_log.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /ping_logs/1
-  # DELETE /ping_logs/1.json
+  # DELETE /servers/1/ping_logs/1
+  # DELETE /servers/1/ping_logs/1.json
   def destroy
-    @ping_log = PingLog.find(params[:id])
+    @server = Server.find(params[:server_id])
+    @ping_log = @server.ping_logs.find(params[:id])
     @ping_log.destroy
 
     respond_to do |format|
-      format.html { redirect_to ping_logs_url }
+      format.html { redirect_to server_ping_logs_path(@server) }
       format.json { head :no_content }
     end
   end
