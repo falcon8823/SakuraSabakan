bbac3cf8b858e9d2bebb60f435c6a4cc321201e7
Hayato OKUMOTO <falcon@falconsrv.net>
2013-03-18 15:56:12 +0900

    Add contact page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ ServerWatcher::Application.routes.draw do
 
   root to: 'welcome#index'
 
+  get '/contact', to: 'welcome#contact'
+
   resources :servers do
     resources :ping_logs, only: [:index, :show, :destroy]
     resources :httping_logs, only: [:index, :show, :destroy]
