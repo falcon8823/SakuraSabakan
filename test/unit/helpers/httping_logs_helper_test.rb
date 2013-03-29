36f637b51a295f04ffbbca99be18fde94182b79b
Hayato OKUMOTO <falcon@falconsrv.net>
2013-03-29 19:02:23 +0900

    Remove test files

--- test/unit/helpers/httping_logs_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class HttpingLogsHelperTest < ActionView::TestCase
-end
--- test/unit/ping_log_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class PingLogTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
