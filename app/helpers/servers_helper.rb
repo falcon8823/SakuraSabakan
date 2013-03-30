de7144d89df659a1adaec4714ab4d21cdf58f9fc
Hayato OKUMOTO <falcon@falconsrv.net>
2013-03-30 14:04:36 +0900

    Fix server view and helper

--- app/helpers/servers_helper.rb
+++ app/helpers/servers_helper.rb
@@ -18,6 +18,14 @@ module ServersHelper
     danger: 'exclamation-sign'
   }
 
+  def status_icon_tag(status)
+    icon_tag "icon-#{ICON[status]}"
+  end
+
+  def rate_tag(rate, status)
+    content_tag :span, "#{rate}%", class: "label #{LABEL[status]}"
+  end
+
   # ステータス表示付きのアコーディオンヘッダタグ
   def status_accordion_toggle_tag(status, href, &block)
     content_tag :a,
@@ -27,39 +35,5 @@ module ServersHelper
       &block
   end
 
-  # pingの稼働率の色つきタグ
-  def ping_rate_tag(server)
-    rate = server.recent_ping_rate 1.day
-    status = server.ping_status_before 1.day
 
-    content_tag :span, "#{rate}%", class: "label #{LABEL[status]}"
-  end
-
-  # pingの状態アイコンタグ
-  def ping_status_icon_tag(server)
-    status = server.ping_status_before 1.day
-
-    icon_tag "icon-#{ICON[status]}"
-  end
-
-  # httpの稼働率の色つきタグ
-  def http_rate_tag(server)
-    rate = server.recent_http_rate 1.day
-    status = server.http_status_before 1.day
-
-    content_tag :span, "#{rate}%", class: "label #{LABEL[status]}"
-  end
-
-  # httpの状態アイコンタグ
-  def http_status_icon_tag(server)
-    status = server.http_status_before 1.day
-
-    icon_tag "icon-#{ICON[status]}"
-  end
-
-  def server_status_icon_tag(server)
-    status = server.status_before 1.day
-
-    icon_tag "icon-#{ICON[status]}"
-  end
 end
